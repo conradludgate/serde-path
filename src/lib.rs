5ce6380957ee9d67f6d513f44c1ddf352b782f8a
Conrad Ludgate <conradludgate@gmail.com>
2025-06-28 10:17:08 +0100

    generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,52 @@
-use std::marker::PhantomData;
+mod list;
+mod map;
 
 use serde::de;
+
+pub trait FilterChain {
+    fn filter<'de, S, D>(&self, seed: S, deserializer: D) -> Result<S::Value, D::Error>
+    where
+        S: de::DeserializeSeed<'de>,
+        D: de::Deserializer<'de>;
+}
+
+impl<F: FilterChain + ?Sized> FilterChain for &F {
+    fn filter<'de, S, D>(&self, seed: S, deserializer: D) -> Result<S::Value, D::Error>
+    where
+        S: de::DeserializeSeed<'de>,
+        D: de::Deserializer<'de>,
+    {
+        F::filter(self, seed, deserializer)
+    }
+}
+
+struct Final;
+
+impl FilterChain for Final {
+    fn filter<'de, S, D>(&self, seed: S, deserializer: D) -> Result<S::Value, D::Error>
+    where
+        S: de::DeserializeSeed<'de>,
+        D: de::Deserializer<'de>,
+    {
+        seed.deserialize(deserializer)
+    }
+}
+
+impl<F1: FilterChain, F2: FilterChain> FilterChain for (F1, F2) {
+    fn filter<'de, S, D>(&self, seed: S, deserializer: D) -> Result<S::Value, D::Error>
+    where
+        S: de::DeserializeSeed<'de>,
+        D: de::Deserializer<'de>,
+    {
+        self.0.filter(
+            Chain {
+                filter: &self.1,
+                seed,
+            },
+            deserializer,
+        )
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 pub enum Field<'a> {
@@ -8,19 +54,40 @@ pub enum Field<'a> {
     Key(&'a str),
 }
 
-pub struct Path<'a, S> {
-    path: &'a [Field<'a>],
-    seed: S,
-}
-
-impl<'a, S> Path<'a, S> {
-    pub fn new(path: &'a [Field<'a>], seed: S) -> Self {
-        Self { path, seed }
+impl FilterChain for Field<'_> {
+    fn filter<'de, S, D>(&self, seed: S, deserializer: D) -> Result<S::Value, D::Error>
+    where
+        S: de::DeserializeSeed<'de>,
+        D: de::Deserializer<'de>,
+    {
+        match self {
+            Field::Index(head) => head.filter(seed, deserializer),
+            Field::Key(head) => head.filter(seed, deserializer),
+        }
     }
 }
 
-impl<'de, S> de::DeserializeSeed<'de> for Path<'_, S>
+impl<F: FilterChain> FilterChain for [F] {
+    fn filter<'de, S, D>(&self, seed: S, deserializer: D) -> Result<S::Value, D::Error>
+    where
+        S: de::DeserializeSeed<'de>,
+        D: de::Deserializer<'de>,
+    {
+        match self {
+            [] => Final.filter(seed, deserializer),
+            [head, filter @ ..] => head.filter(Chain { filter, seed }, deserializer),
+        }
+    }
+}
+
+struct Chain<'a, F: ?Sized, S> {
+    filter: &'a F,
+    seed: S,
+}
+
+impl<'de, F, S> de::DeserializeSeed<'de> for Chain<'_, F, S>
 where
+    F: ?Sized + FilterChain,
     S: de::DeserializeSeed<'de>,
 {
     type Value = S::Value;
@@ -29,151 +96,42 @@ where
     where
         D: de::Deserializer<'de>,
     {
-        let Self { path, seed } = self;
-        match path {
-            [] => seed.deserialize(deserializer),
-            &[head, ref path @ ..] => {
-                let next = Self { path, seed };
-                match head {
-                    Field::Index(head) => deserializer.deserialize_seq(ListVisitor { head, next }),
-                    Field::Key(head) => deserializer.deserialize_map(MapVisitor { head, next }),
-                }
-            }
-        }
+        let Self { filter: path, seed } = self;
+        path.filter(seed, deserializer)
     }
 }
 
-struct ListVisitor<'a, T> {
-    head: usize,
-    next: Path<'a, T>,
-}
-
-impl<'de, S> de::Visitor<'de> for ListVisitor<'_, S>
-where
-    S: de::DeserializeSeed<'de>,
-{
-    type Value = S::Value;
-    fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "a sequence containing element {:?}", self.head)
-    }
-
-    #[inline]
-    fn visit_seq<A: de::SeqAccess<'de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
-        let Self { head, next } = self;
-        let mut i = 0;
-        let v = loop {
-            if i == head {
-                break seq.next_element_seed(next)?;
-            }
-
-            if seq.next_element::<de::IgnoredAny>()?.is_none() {
-                break None;
-            }
-            i += 1;
-        };
-
-        let v = v.ok_or_else(|| de::Error::custom(format_args!("missing field `{head:?}`")))?;
-
-        while seq.next_element::<de::IgnoredAny>()?.is_some() {}
-        Ok(v)
-    }
-}
-
-struct MapVisitor<'a, T> {
-    head: &'a str,
-    next: Path<'a, T>,
-}
-
-impl<'de, S> de::Visitor<'de> for MapVisitor<'_, S>
-where
-    S: de::DeserializeSeed<'de>,
-{
-    type Value = S::Value;
-    fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "a map containing element {:?}", self.head)
-    }
-
-    #[inline]
-    fn visit_map<A: de::MapAccess<'de>>(self, mut map: A) -> Result<Self::Value, A::Error> {
-        let Self { head, next } = self;
-        let visitor = FieldVisitor(head);
-
-        let v = loop {
-            let Some(found) = map.next_key_seed(visitor)? else {
-                break None;
-            };
-
-            if found {
-                break Some(map.next_value_seed(next)?);
-            }
-
-            map.next_value::<de::IgnoredAny>()?;
-        };
-
-        let v = v.ok_or_else(|| de::Error::custom(format_args!("missing field `{head}`")))?;
-
-        while let Some((found, _)) = map.next_entry_seed(visitor, PhantomData::<de::IgnoredAny>)? {
-            if found {
-                return Err(de::Error::custom(format_args!("duplicate field `{head}`")));
-            }
-        }
-
-        Ok(v)
-    }
-}
-
-#[derive(Clone, Copy)]
-struct FieldVisitor<'a>(&'a str);
-
-impl<'de> de::Visitor<'de> for FieldVisitor<'_> {
-    type Value = bool;
-    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        std::fmt::Formatter::write_str(formatter, "field identifier")
-    }
-
-    #[inline]
-    fn visit_str<E: de::Error>(self, v: &str) -> Result<Self::Value, E> {
-        self.visit_bytes(v.as_bytes())
-    }
-
-    #[inline]
-    fn visit_bytes<E: de::Error>(self, v: &[u8]) -> Result<Self::Value, E> {
-        Ok(v == self.0.as_bytes())
-    }
-}
-
-impl<'de> de::DeserializeSeed<'de> for FieldVisitor<'_> {
-    type Value = bool;
-
-    #[inline]
-    fn deserialize<D>(self, deserializer: D) -> Result<bool, D::Error>
-    where
-        D: de::Deserializer<'de>,
-    {
-        deserializer.deserialize_identifier(self)
-    }
+#[macro_export]
+macro_rules! hlist {
+    [$head:expr $(, $expr:expr)* $(,)?] => {
+        ($head, $crate::hlist![$($expr,)*])
+    };
+    [] => { $crate::Final };
 }
 
 #[cfg(test)]
 mod tests {
-    use serde::de::DeserializeSeed;
     use serde_json::{json, value::RawValue};
 
-    use crate::{Field, Path};
+    use crate::FilterChain;
 
-    fn extract_json_path<'de, T>(json: &'de str, path: &[Field]) -> Result<T, serde_json::Error>
+    fn extract_json_path<'de, T>(
+        json: &'de str,
+        filter: &impl FilterChain,
+    ) -> Result<T, serde_json::Error>
     where
         T: serde::Deserialize<'de>,
     {
-        Path::new(path, std::marker::PhantomData::<T>)
-            .deserialize(&mut serde_json::Deserializer::from_str(json))
+        filter.filter(
+            std::marker::PhantomData::<T>,
+            &mut serde_json::Deserializer::from_str(json),
+        )
     }
 
     #[test]
     fn it_works() {
         let json = json!({ "a": 1, "b": {"c": [2, 3, 4]}, "d": 5}).to_string();
-        let field: &RawValue =
-            extract_json_path(&json, &[Field::Key("b"), Field::Key("c"), Field::Index(1)]).unwrap();
+        let field: &RawValue = extract_json_path(&json, &hlist!["b", "c", 1]).unwrap();
         assert_eq!(field.get(), "3")
     }
 }
---
+++ src/list.rs
@@ -0,0 +1,52 @@
+use serde::de;
+
+use crate::FilterChain;
+
+impl FilterChain for usize {
+    fn filter<'de, S, D>(&self, seed: S, deserializer: D) -> Result<S::Value, D::Error>
+    where
+        S: de::DeserializeSeed<'de>,
+        D: de::Deserializer<'de>,
+    {
+        deserializer.deserialize_seq(ListVisitor {
+            head: *self,
+            next: seed,
+        })
+    }
+}
+
+struct ListVisitor<S> {
+    head: usize,
+    next: S,
+}
+
+impl<'de, S> de::Visitor<'de> for ListVisitor<S>
+where
+    S: de::DeserializeSeed<'de>,
+{
+    type Value = S::Value;
+    fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "a sequence containing element {:?}", self.head)
+    }
+
+    #[inline]
+    fn visit_seq<A: de::SeqAccess<'de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
+        let Self { head, next } = self;
+        let mut i = 0;
+        let v = loop {
+            if i == head {
+                break seq.next_element_seed(next)?;
+            }
+
+            if seq.next_element::<de::IgnoredAny>()?.is_none() {
+                break None;
+            }
+            i += 1;
+        };
+
+        let v = v.ok_or_else(|| de::Error::custom(format_args!("missing field `{head:?}`")))?;
+
+        while seq.next_element::<de::IgnoredAny>()?.is_some() {}
+        Ok(v)
+    }
+}
